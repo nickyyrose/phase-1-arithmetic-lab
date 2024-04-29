831565f11f3ba23141ea247ce0f82bc68efb5e2c
Nicholas Rose <ntrose90@gmail.com>
2024-04-29 00:35:57 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31, num2 = 2;
+const multiply = (num1 * num2);
 
-// Write your code here
+
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 20, num4 = 8;
+const mod = (num3 % num4);
+
+const max = Math.max(0, 4, 20);
